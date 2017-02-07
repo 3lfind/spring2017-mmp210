da9bac6f8e8ae53abad8971e0737a6ab8b3cb742
3lfind <3lfind@gmail.com>
2017-02-07 17:02:37 -0500

    Add files via upload

---
+++ mmp210/week1/sketch.js
@@ -0,0 +1,17 @@
+function setup(){
+    createCanvas(640,480);
+    
+    background('lightgreen');
+    
+    ellipse(300,300,500);
+    
+    rect(170, 175, 50,50);
+    rect(370, 175, 50,50);
+  
+    line(300,250, 300, 330);
+    
+    triangle(250,350,300,400,350,350);
+
+    arc(50,250,80,160, HALF_PI, PI);
+    arc(550,255,80,160,0, HALF_PI);
+}
